a6ea47e7205eac725b3b3dd1d551cc55dd7e4f3f
onamoiloa <onalennatumelo2@gmail.com>
2025-06-10 23:01:27 +0200

    Added the API KEY

---
+++ config.js
@@ -0,0 +1,8 @@
+let rootPath = "https://mysite.itvarsity.org/api/ContactBook";
+let apiKey = checkApiKey();
+
+function checkApiKey() {
+  if (!localStorage.getItem("apiKey")) {
+    window.open("enter-api-key.html", "_self");
+  }
+}
